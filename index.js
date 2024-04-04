a8ada2c391cf8ed625d2bf9ab5c85ba60aaafaf0
Miguel Angel Bohorquez <mmige24@gmail.com>
2024-04-04 17:00:28 -0500

    Se hace responsive en dos tamaños de pantalla

--- index.js
+++ index.js
@@ -1,39 +1,40 @@
 window.addEventListener("scroll", function(){
     var text = document.querySelector(".inicioTexto");
-    text.classList.toggle("abajo",this.window.scrollY>2)
+    text.classList.toggle("abajo",this.window.scrollY>161)
+    console.log("Vertical "+window.scrollY);
 })
 
 
 window.addEventListener("scroll", function(){
     var text2 = document.querySelector(".partTwoText");
-    text2.classList.toggle("partTwoTextActive",this.window.scrollY>256)
+    text2.classList.toggle("partTwoTextActive",this.window.scrollY>525)
 })
 
 window.addEventListener("scroll", function(){
     var text3 = document.querySelector(".partThreeText");
-    text3.classList.toggle("partThreeTextActive",this.window.scrollY>800)
+    text3.classList.toggle("partThreeTextActive",this.window.scrollY>1089)
 })
 
 
 window.addEventListener("scroll", function(){
     var boxe1 = document.querySelector(".active1");
-    boxe1.classList.toggle("boxActive",this.window.scrollY>1835)
+    boxe1.classList.toggle("boxActive",this.window.scrollY>2770)
     var boxe2 = document.querySelector(".active2");
-    boxe2.classList.toggle("boxActive",this.window.scrollY>1835)
+    boxe2.classList.toggle("boxActive",this.window.scrollY>2770)
     var boxe3 = document.querySelector(".active3");
-    boxe3.classList.toggle("boxActive",this.window.scrollY>1835)
+    boxe3.classList.toggle("boxActive",this.window.scrollY>2770)
     var boxe4 = document.querySelector(".active4");
-    boxe4.classList.toggle("boxActive",this.window.scrollY>1835)
+    boxe4.classList.toggle("boxActive",this.window.scrollY>2770)
     var boxe5 = document.querySelector(".active5");
-    boxe5.classList.toggle("boxActive",this.window.scrollY>1835)
+    boxe5.classList.toggle("boxActive",this.window.scrollY>2770)
     var boxe6 = document.querySelector(".active6");
-    boxe6.classList.toggle("boxActive",this.window.scrollY>1835)
+    boxe6.classList.toggle("boxActive",this.window.scrollY>2770)
     var boxe7 = document.querySelector(".active7");
-    boxe7.classList.toggle("boxActive",this.window.scrollY>1835)
+    boxe7.classList.toggle("boxActive",this.window.scrollY>2770)
     var boxe8 = document.querySelector(".active8");
-    boxe8.classList.toggle("boxActive",this.window.scrollY>1835)
+    boxe8.classList.toggle("boxActive",this.window.scrollY>2770)
     var boxe9 = document.querySelector(".active9");
-    boxe9.classList.toggle("boxActive",this.window.scrollY>1835)
+    boxe9.classList.toggle("boxActive",this.window.scrollY>2770)
 })
 
 
